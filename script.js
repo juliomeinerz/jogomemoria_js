36eb75272ec1b4ee996e4e2a073338f00173b91f
= <julio.meinerz@live.com>
2016-11-16 11:32:02 -0200

    adicionadas funcionalidades, acerto e erro

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
+var dificuldade = 0;
+var categoria = null;
+var jogada = null;
+var previousId = null;
+console.log(jogada);
+
 //INICIALIZA O JOGO
 var game = {
 	getDificuldade: function() {
-		var dificuldade = 0;
 		for (var i = 0; i < components.selectDificuldade.length; i++) {
 			if(components.selectDificuldade[i].checked) {
 				dificuldade = components.selectDificuldade[i].value;
@@ -11,41 +16,86 @@ var game = {
 		return dificuldade;
 	},
 	getCategoria: function() {
-		var categoria = 0;
 		for (var i = 0; i < components.selectCategoria.length; i++) {
 			if(components.selectCategoria[i].checked) {
 				categoria = components.selectCategoria[i].value;				
 			}
 		}	
 		if (categoria == "natureza") {
+			categoria = natureza;
 			return natureza;
 		}
 		else {
 			if(categoria == "carros") {
+				categoria = carros;
 				return carros;
 			}
 			else {
+				categoria = desenhos;
 				return desenhos;
 			}
 		}
 	},
-	start: function(dificuldade,categoria) {	
+	start: function(dificuldade,categoria) {		
+		jogada = null;
 		this.trocaTela(components.telaDificuldade,components.telaJogo);	
 		pecas.createAll(dificuldade);
-		pecas.showAll(categoria);
+		pecas.identifyPecas();
+		pecas.populateAll(categoria);		
 		pecas.hideAll(dificuldade);
 	},	
 	trocaTela: function(current,next) {
 		current.style.display = "none";
 		next.style.display = "block";	
+		imagensMisturadas = shuffle();
 	},	
 
 };
 //EVENTOS REFERENTES ÀS PEÇAS 
+console.log(categoria);
+
 var pecas = {
-	showSelected: function(peca) {
-		alert(peca);
+	compare: function(jogadaUsuario,idPeca) {
+		if (jogada == null) {
+			jogada = jogadaUsuario;
+			previousId = idPeca;
+			document.getElementById(idPeca).src = categoria[imagensMisturadas[jogadaUsuario]];
+			console.log(jogada);
+			console.log(previousId);
+		}
+
+		else {
+			console.log(jogadaUsuario);
+			document.getElementById(idPeca).src = categoria[imagensMisturadas[jogadaUsuario]];
+			if(jogada == jogadaUsuario) {				
+				console.log("Você acertou");
+				components.peca[idPeca].removeAttribute("onclick");		
+				components.peca[previousId].removeAttribute("onclick");			
+				jogada = null;
+				previousId = null;
+			}
+			else {				
+				console.log("Você errou");		
+				this.hideWrong(previousId,idPeca);		
+				jogada = null;	
+				previousId = null;
+			}
+		}
+	},	
+	hideWrong: function(pecaUm, pecaDois) {
+		setTimeout(function() {
+			document.getElementById(pecaUm).src = defaultImage;
+			document.getElementById(pecaDois).src = defaultImage;
+		},1500);
 	},
+	identifyPecas: function() {
+		for (var i = 0; i < components.image.length; i++) {
+			components.image[i].id = i;
+		}
+	},
+
+	
+	
 	hideAll: function(dificuldade) {
 		var interval = dificuldade * 3000;
 		var hideAll = setTimeout(function() {
@@ -54,28 +104,27 @@ var pecas = {
 			}	
 		}, interval);
 	},	
-	showAll: function(categoria) {				
-				shuffle();		
-				var numPecas = components.peca.length;
-				var pecasMisturadas = shuffle();	
-				for(var i = 0; i < numPecas/2; i++) {		
-					components.image[pecasMisturadas[i]].src = categoria[i];				
-				}	
-				for(var i = 0; i < numPecas/2; i++) {		
-					components.image[pecasMisturadas[i+(numPecas/2)]].src = categoria[i];				
-				}			
+	populateAll: function(categoria) {	
+			var numPecas = components.peca.length;
+			for(var i = 0; i < numPecas; i++) {
+				var image = document.getElementById(i);
+				var src = categoria.indexOf(categoria[imagensMisturadas[i]]);
+				image.src = categoria[imagensMisturadas[i]];				
+				components.peca[i].setAttribute("onclick", "pecas.compare(" + src + ","+ i +")");
+			}	
 	},
+
+
+
 	createAll: 	function(dificuldade) {
 			for (var i = 0; i < dificuldade; i++) {
-				for (var j = 0; j < 12; j++) {
+				for (var j = 0; j < 12; j++) {					
 					var peca = document.createElement("div");
 					peca.className = "peca";
-					peca.id = j;
-					peca.setAttribute("onclick", "pecas.showSelected(" + j + ")");				
 					components.telaJogo.appendChild(peca);				
 					var img = document.createElement("IMG");
 					peca.appendChild(img);
-					img.name = "peca";				
+					img.name = "peca";		
 				}			
 			}
 	},
@@ -165,13 +214,22 @@ var components = {
 
 function shuffle() {
 		var array = [];
-		for (var i = 0; i < components.peca.length; i++) {
-			array.push(i);
-		}		
+
+		var random = Math.floor(Math.random() * (categoria.length / 3) * dificuldade));
+		console.log(random)
+		
+		console.log(dificuldade);
+		for(var i = 0; i < 2; i++) {
+			for (var j = 0; j < ((categoria.length/3) * dificuldade); j++) {
+				
+			}			
+		}
+		
 		for (var i = array.length; i; i--) {
         	var j = Math.floor(Math.random() * i);
        		[array[i - 1], array[j]] = [array[j], array[i - 1]];
-   		}
-   		console.log(array);
+   		}   		
    		return array;
 };
+
+
